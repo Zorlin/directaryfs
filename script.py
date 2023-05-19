123bf6b05feafbf01c8b53c83e404b36f9ee2cbf
Benjamin Arntzen <zorlin@gmail.com>
2023-05-19 08:25:37 +0800

    Better memory management

--- script.py
+++ script.py
@@ -9,14 +9,16 @@ class DirectaryFS(LoggingMixIn, Operations):
     def __init__(self, root):
         self.root = root
         self.fd = 0
-        self.data = {}
         self.attr = {}
         self.files = [f for f in os.listdir(root) if os.path.isdir(os.path.join(root, f))]
         total_dirs = len(self.files)
         start_time = time.time()
         for idx, file in enumerate(self.files):
             dir_path = os.path.join(root, file)
-            tar_size = sum(os.path.getsize(os.path.join(dir_path, f)) for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f)))
+            with io.BytesIO() as buf:
+                with tarfile.open(fileobj=buf, mode='w') as tar:
+                    tar.add(dir_path, arcname='')
+                tar_size = len(buf.getvalue())
             self.attr[file] = dict(st_mode=(0o100644 | 32768), st_nlink=1, st_size=tar_size)
             elapsed_time = time.time() - start_time
             estimated_time_remaining = (elapsed_time / (idx + 1)) * (total_dirs - idx - 1)
@@ -34,18 +36,14 @@ class DirectaryFS(LoggingMixIn, Operations):
 
     def open(self, path, flags):
         self.fd += 1
-        if path[1:] not in self.data:
-            buf = io.BytesIO()
-            with tarfile.open(fileobj=buf, mode='w') as tar:
-                tar.add(os.path.join(self.root, path[1:]), arcname='')
-            buf.seek(0)
-            self.data[path[1:]] = buf
         return self.fd
 
     def read(self, path, size, offset, fh):
-        buf = self.data[path[1:]]
-        buf.seek(offset)
-        return buf.read(size)
+        with io.BytesIO() as buf:
+            with tarfile.open(fileobj=buf, mode='w') as tar:
+                tar.add(os.path.join(self.root, path[1:]), arcname='')
+            buf.seek(offset)
+            return buf.read(size)
 
 
 if __name__ == "__main__":
